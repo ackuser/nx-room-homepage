942bf1eba2713604b1a913501d880ffcc283198e
ackuser <mail@ackarim.com>
2021-03-10 08:17:09 +0100

    Initial commit for nx project https://bit.ly/3l072UF

---
+++ apps/room-homepage/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'room-homepage-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'room-homepage';
+}
